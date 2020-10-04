22d741764814a25aa5991ba3831403b037d3e852
jayisaac0 <jayisaac0@gmail.com>
2020-10-04 17:44:10 +0300

    first commit

--- src/services/Slider.js
+++ src/services/Slider.js
@@ -5,7 +5,7 @@ export const coverSlider = [
         "year": "2012",
         "description": "",
         "cover": "https://wonderfulengineering.com/wp-content/uploads/2013/11/transformers-wallpapers-6.jpg",
-        "video": "http://s1.dlserver.info/Movie/Transformers.2007/Transformers.2007.720p.mkv"
+        "video": "https://s1.dlserver.info/Movie/Transformers.2007/Transformers.2007.720p.mkv"
     },
     {
         "id": 2,
@@ -13,7 +13,7 @@ export const coverSlider = [
         "year": "2013",
         "description": "",
         "cover": "https://wallpaperaccess.com/full/1653479.jpg",
-        "video": "http://s1.dlserver.info/Movie/Thor.The.Dark.World.2013/Thor.The.Dark.World.2013.1080p.3D.HSBS.YIFY.mp4"
+        "video": "https://s1.dlserver.info/Movie/Thor.The.Dark.World.2013/Thor.The.Dark.World.2013.1080p.3D.HSBS.YIFY.mp4"
     },
     {
         "id": 3,
@@ -21,7 +21,7 @@ export const coverSlider = [
         "year": "2013",
         "description": "",
         "cover": "https://cdn.guidingtech.com/media/assets/_1200x630_crop_center-center_82_none/1_pacific_rim-HD-Wallpapers.jpg?mtime=1520485612",
-        "video": "http://185.105.101.222/2018/film/Pacific%20Rim%20Uprising%202018/Pa.cific.Ri.m.Up.rising.2018.720p.b.lu.ry.x265.HEVC.MkvCage.Filmha_CO.mkv"
+        "video": "https://185.105.101.222/2018/film/Pacific%20Rim%20Uprising%202018/Pa.cific.Ri.m.Up.rising.2018.720p.b.lu.ry.x265.HEVC.MkvCage.Filmha_CO.mkv"
     },
     {
         "id": 4,
@@ -29,7 +29,7 @@ export const coverSlider = [
         "year": "2013",
         "description": "",
         "cover": "https://wallpaperaccess.com/full/1653479.jpg",
-        "video": "http://s1.dlserver.info/Movie/Thor.The.Dark.World.2013/Thor.The.Dark.World.2013.1080p.3D.HSBS.YIFY.mp4"
+        "video": "https://s1.dlserver.info/Movie/Thor.The.Dark.World.2013/Thor.The.Dark.World.2013.1080p.3D.HSBS.YIFY.mp4"
     },
     {
         "id": 5,
@@ -37,6 +37,6 @@ export const coverSlider = [
         "year": "2013",
         "description": "",
         "cover": "https://wallpaperaccess.com/full/1653479.jpg",
-        "video": "http://s1.dlserver.info/Movie/Thor.The.Dark.World.2013/Thor.The.Dark.World.2013.1080p.3D.HSBS.YIFY.mp4"
+        "video": "https://s1.dlserver.info/Movie/Thor.The.Dark.World.2013/Thor.The.Dark.World.2013.1080p.3D.HSBS.YIFY.mp4"
     }
 ]
